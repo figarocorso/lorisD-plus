ad895d287794ca5ba3cc526be974c3c2cd8c172d
figarocorso <figarocorso@gmail.com>
2026-01-29 19:11:51 +0100

    Add more Loris images

--- lorisImages.js
+++ lorisImages.js
@@ -1,4 +1,8 @@
 var lorisImages = [
     "https://i.imgur.com/RY0hqI7.jpeg",
     "https://i.imgur.com/KA01fQZ.jpeg",
+    "https://i.imgur.com/Blic438.jpeg",
+    "https://i.imgur.com/VM1F7XY.jpeg",
+    "https://i.imgur.com/YVzUn2A.jpeg",
+    "https://i.imgur.com/clDvnPU.jpeg",
 ];
